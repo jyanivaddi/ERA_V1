6186e4d39a394daddd5fee83d0f3f29a6d94a2ce
Jyani Vaddi <Jyani.Vaddi@gmail.com>
2023-06-26 23:06:43 +0530

    basic skeleton

--- session_9/s9_model.py
+++ session_9/s9_model.py
@@ -4,7 +4,6 @@ import torch.nn.functional as F
 from torchsummary import summary
 from tqdm import tqdm
 
-
 def model_summary(model, input_size):
     summary(model, input_size = input_size)
 
@@ -32,7 +31,6 @@ def model_train(model, device, train_loader, optimizer, train_acc, train_losses)
     return  loss.item()
 
 
-
 def model_test(model, device, test_loader, test_acc, test_losses):
     model.eval()
     test_loss = 0
@@ -54,121 +52,44 @@ def model_test(model, device, test_loader, test_acc, test_losses):
     test_losses.append(test_loss)
     return test_loss
 
-# Transition Block 1
-# Max Pool
-# input_size: 24 output_size = 12 
-# rf_in: 5, s = 2, j_in = 1, j_out = 2, rf_out = 6
 
-
-
-class Model_Net(nn.Module):
-
-    def __init__(self, norm_type = 'batch_norm',num_groups = 1):
-        super().__init__()
-
-        self.NUM_GROUPS=num_groups
-        #norm_type='batch_norm'
-
-        if norm_type == 'batch_norm':
-            self.norm_func = nn.BatchNorm2d
-            self.norm_args = [] 
-        elif norm_type == 'layer_norm':
-            self.norm_func = nn.GroupNorm
-            self.norm_args = [1]
+class Block(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size = 3, drop_out_probability=0.05, use_pool=False):
+        super(Block, self).__init__()
+        self.drop_out_probability = drop_out_probability
+        self.conv1 = self.single_convolution(in_channels, out_channels, kernel_size=kernel_size)
+        self.conv2 = self.single_convolution(out_channels, out_channels*2,kernel_size=kernel_size)
+        if use_pool:
+            self.conv3 = self.transition_block_with_max_pool(out_channels*2, out_channels)
         else:
-            self.norm_func = nn.GroupNorm
-            self.norm_args = [self.NUM_GROUPS]
+            self.conv3 = self.transition_block_wo_max_pool(out_channels*2, out_channels)
 
-        self.drop_out_probability = 0.02
-
-        # input_size: 32 output_size = 32 
-        # rf_in: 1, k=3, s = 1, p=1, j_in = 1, j_out = 1, rf_out = 3
-        self.conv_block_1_1 = self.conv_block(3, 16, padding=1) 
-
-        # input_size: 32 output_size = 32 
-        # rf_in: 3, k=3, s = 1, p=1, j_in = 1, j_out = 1, rf_out = 5
-        self.conv_block_1_2 = self.conv_block(16, 32, padding=1) 
-
-        # Max Pool
-        # input_size: 32 output_size = 16 
-        # rf_in: 5, k=2, s = 2, p=0, j_in = 1, j_out = 2, rf_out = 6
-        # 1 x 1 
-        # input_size: 16 output_size = 16 
-        # rf_in: 6, k=1, s = 1, p=0, j_in = 2, j_out = 2, rf_out = 6
-        self.transition_block_1 = self.transition_block_with_max_pool(32,16) 
-
-        # input_size: 16 output_size = 16 
-        # rf_in: 6, k=3, s = 1, p=1, j_in = 2, j_out = 2, rf_out = 10
-        self.conv_block_2_1 = self.conv_block(16,32, padding=1) 
-
-        # input_size: 16 output_size = 16 
-        # rf_in: 10, k=3, s = 1, p=1, j_in = 2, j_out = 2, rf_out = 14
-        self.conv_block_2_2 = self.conv_block(32,32, padding=1) 
-
-        # input_size: 16 output_size = 16 
-        # rf_in: 14, k=3, s = 1, p=1, j_in = 2, j_out = 2, rf_out = 18
-        self.conv_block_2_3 = self.conv_block(32,32, padding=1) 
-
-        # Max Pool
-        # input_size: 16 output_size = 8 
-        # rf_in: 18, k=2, s = 2, p=0, j_in = 2, j_out = 4, rf_out = 20
-        # 1 x 1 
-        # input_size: 8 output_size = 8 
-        # rf_in: 20, k=1, s = 1, p=0, j_in = 4, j_out = 4, rf_out = 20        
-        self.transition_block_2 = self.transition_block_with_max_pool(32,16) 
-
-        # input_size: 8 output_size = 8 
-        # rf_in: 20, k=3, s = 1, p=1, j_in = 4, j_out = 4, rf_out = 28
-        self.conv_block_3_1 = self.conv_block(16,16, padding=1) 
-
-        # input_size: 8 output_size = 8 
-        # rf_in: 28, k=3, s = 1, p=1, j_in = 4, j_out = 4, rf_out = 36
-        self.conv_block_3_2 = self.conv_block(16,32, padding=0)  
-
-        # input_size: 8 output_size = 8 
-        # rf_in: 36, k=3, s = 1, p=1, j_in = 4, j_out = 4, rf_out = 44
-        self.conv_block_3_3 = self.conv_block(32,32, padding=0) 
-
-        # AAP
-        # input_size: 8 output_size = 1 
-        # rf_in: 44, k=1, s = 1, p=0, j_in = 4, j_out = 4, rf_out = 44        
-        self.aap = nn.AdaptiveAvgPool2d((1,1)) 
-
-        # 1 x 1 
-        # input_size: 1 output_size = 1 
-        # rf_in: 44, k=1, s = 1, p=0, j_in = 4, j_out = 4, rf_out = 44        
-        self.transition_block_3 = self.transition_block_wo_max_pool(32,10)
-
-    def conv_block(self, in_channels, out_channels, kernel_size=3, padding=0):
-        self.norm_args.append(out_channels)
+    def single_convolution(self,in_channels, out_channels,kernel_size, padding=1):
         # Define Conv Block
         conv_block = nn.Sequential(
             nn.Conv2d(in_channels=in_channels, 
-                      out_channels=out_channels, 
-                      kernel_size=(kernel_size, kernel_size), 
-                      padding=padding, 
-                      bias=False),
-            self.norm_func(*self.norm_args),
+                    out_channels=out_channels, 
+                    kernel_size=(kernel_size, kernel_size), 
+                    padding=padding, 
+                    bias=False),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(),
             nn.Dropout(self.drop_out_probability)
         )
-        self.norm_args.pop(-1)
         return conv_block
-
+    
     def transition_block_with_max_pool(self, in_channels, out_channels):
-        self.norm_args.append(out_channels)
         transition_block = nn.Sequential(
             nn.Conv2d(in_channels=in_channels, 
                         out_channels= out_channels,
                         kernel_size = (1,1),
                         padding=0,
                         bias=False),
-            self.norm_func(*self.norm_args),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(),
             nn.Dropout(self.drop_out_probability),
             nn.MaxPool2d(2,2),
         )
-        self.norm_args.pop(-1)
         return transition_block
 
     def transition_block_wo_max_pool(self, in_channels, out_channels):
@@ -180,20 +101,46 @@ class Model_Net(nn.Module):
                         bias=False),
         )
         return transition_block
+    
+    
+    def __call__(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        return x
 
+# Transition Block 1
+# Max Pool
+# input_size: 24 output_size = 12 
+# rf_in: 5, s = 2, j_in = 1, j_out = 2, rf_out = 6
+
+class Model_Net(nn.Module):
+
+    def __init__(self, base_channels = 3, num_classes = 10, drop_out = 0.05):
+        super(Model_Net,self).__init__()
+        self.drop_out_probability = drop_out
+        self.num_classes = num_classes
+
+        self.block_1_in_channels = base_channels
+        self.block_1_out_channels = 32
+        self.block1 = Block(self.block_1_in_channels,self.block_1_out_channels,drop_out_probability=self.drop_out_probability, use_pool=True)
+        
+        self.block_2_in_channels = 32
+        self.block_2_out_channels = 32
+        self.block2 = Block(self.block_2_in_channels,self.block_2_out_channels,drop_out_probability=self.drop_out_probability, use_pool=True)
+
+        self.block_3_in_channels = 32
+        self.block_3_out_channels = 32
+        self.block3 = Block(self.block_3_in_channels,self.block_3_out_channels,drop_out_probability=self.drop_out_probability, use_pool=False)
+
+        self.aap = nn.AdaptiveAvgPool2d(1)
+        self.final = nn.Conv2d(self.block_3_out_channels,num_classes, kernel_size=(1,1),bias=False, padding=0) 
     
     def forward(self, x):
-        x = self.conv_block_1_1(x)
-        x = self.conv_block_1_2(x)
-        x = self.transition_block_1(x)
-        x = self.conv_block_2_1(x)
-        x = self.conv_block_2_2(x)
-        x = self.conv_block_2_3(x)
-        x = self.transition_block_2(x)
-        x = self.conv_block_3_1(x)
-        x = self.conv_block_3_2(x)
-        x = self.conv_block_3_3(x)
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
         x = self.aap(x)
-        x = self.transition_block_3(x)
-        x = x.view(-1,10)
+        x = self.final(x)
+        x = x.view(-1,self.num_classes)
         return F.log_softmax(x)
